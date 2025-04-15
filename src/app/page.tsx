4f3286d434241fbf85b77449fbc4c1a14e588071
nandaditra <nandaditra56@gmail.com>
2025-04-15 15:01:34 +0700

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import Header from "./ui/header";
 import ListTable from "./ui/dashboard/listtable";
@@ -19,32 +18,34 @@ export default function Home() {
 
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 dark:bg-black">
        <main className="">
             <div className="flex flex-column">
               <Sidebar />
               <div  className="w-5/6">
                   <Header />
 
-                  <main className="md:px-4 mt-4">
-                    <div className="container mx-auto md:px-16 pt-4 md:pb-16 rounded-md shadow-md bg-white">
-                         <h1 className="text-black font-bold text-2xl mt-5">Dashboard</h1>
+                  <main className="md:px-2 mt-4">
+                    <div className="container mx-auto md:px-10 pt-4 md:pb-16 rounded-md shadow-md bg-white dark:bg-slate-900">
+                         <h1 className="text-black font-bold text-2xl mt-5 dark:text-white">Dashboard</h1>
                          <p className="text-slate-500">List all the phising link</p>
                          <table className="table-auto mt-4 text-center w-full">
-                            <thead className="border-b-1 border-gray-500 py-4">
+                            <thead className="border-b-1 border-gray-500 py-4 dark:text-white">
                               <tr className="text-sm">
                                 <th className="w-4 pb-2">No</th>
                                 <th className="w-12 pb-2">Title</th>
                                 <th className="w-24 pb-2">Url</th>
                                 <th className="w-10 pb-2">Domain</th>
-                                <th className="w-16 pb-2">Created At</th>
-                                <th className="w-16 pb-2">Updated At</th>
+                                <th className="w-10 pb-2">Created At</th>
+                                <th className="w-10 pb-2">Updated At</th>
+                                <th className="w-10 pb-2">Dos Time</th>
                               </tr>
                             </thead>
                             <tbody className="text-sm">
                               {
                                 items.map((data, i)=> 
                                  <ListTable
+                                   key={i}
                                    id={i}
                                    name={data["name"]}
                                    url={data["url"]}
